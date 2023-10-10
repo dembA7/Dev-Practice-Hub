8f9106e6d94047834a1546c559b58a51e075ae48
unknown <a01709522@tec.mx>
2023-10-10 00:33:58 -0600

    add image on MainActivity

--- Kotlin/App Development/Test App/app/src/main/java/com/example/mytestapplication/MainActivity.kt
+++ Kotlin/App Development/Test App/app/src/main/java/com/example/mytestapplication/MainActivity.kt
@@ -2,12 +2,14 @@ package com.example.mytestapplication
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.mytestapplication.databinding.ActivityMainBinding
 
 class MainActivity: Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            val binding = ActivityMainBinding.inflate(layoutInflater)
-            setContentView(binding.root)
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
